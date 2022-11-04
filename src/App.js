e41886d0341fce7446091567e122b95cfcbb35e0
Lokesh Poovaragan <dude.1996@live.com>
2022-11-04 08:01:54 +0000

    test push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Spheron <code>Upload</code> action
+        </p>
+        <a
+          className="App-link"
+          href="https://spheron.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Spheron
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
